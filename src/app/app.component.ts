d9d3f64d7a0083715167f3d76ff549e0561b1ee0
yshevc <yshevc@softserveinc.com>
2020-11-12 19:50:41 +0200

    lesson1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  name = 'Yurii';
+
+  number = 10;
+
+  arrNumber = [10, 40, 49];
+
+  products = [{
+    name: 'Product 1',
+    price: 20,
+    isStock: true
+  }, {
+    name: 'Product 2',
+    price: 30,
+    isStock: true
+  }, {
+    name: 'Product 3',
+    price: 10,
+    isStock: false
+  }, {
+    name: 'Product 4',
+    price: 27,
+    isStock: true
+  }];
+
+  isOpenMenu = false;
+
+  openDropdown() {
+    this.isOpenMenu = !this.isOpenMenu;
+  }
+
+  addProduct() {
+    this.products.push({
+      name: 'Product 5',
+      price: 35,
+      isStock: true
+    });
+  }
 }
