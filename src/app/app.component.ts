b783670e0be0c8288d5dfd071d6dddbb19066bbe
yshevc <yshevc@softserveinc.com>
2020-11-26 18:40:54 +0200

    done homework-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+
+  view: string = 'grid';
+
+  products: Product[] = [{
+    name: 'Product title 1',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eligendi possimus fugit laudantium   cupiditate quidem aut nisi et, obcaecati, iste quis ducimus ad minus laborum optio? Aperiam nobis    voluptatum totam.',
+    price: 34
+  }, {
+    name: 'Product title 2',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eligendi possimus fugit laudantium   cupiditate quidem aut nisi et, obcaecati, iste quis ducimus ad minus laborum optio? Aperiam nobis    voluptatum totam.',
+    price: 21
+  }];
+
+  changeView(view: string) {
+    this.view = view;
+  }
 }
